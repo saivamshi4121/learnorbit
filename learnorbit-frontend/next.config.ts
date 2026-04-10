4ad4961ee05e69f2ae91237d5c616353198174c7
ashritha89 <ashrithakattamuri123@gmail.com>
2026-04-10 12:52:21 +0530

    Resolved the login issues

--- learnorbit-frontend/next.config.ts
+++ learnorbit-frontend/next.config.ts
@@ -2,14 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
+    const backendUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://127.0.0.1:5000";
     return [
       {
         source: "/uploads/:path*",
-        destination: "http://127.0.0.1:5000/uploads/:path*",
+        destination: `${backendUrl}/uploads/:path*`,
       },
       {
         source: "/api/:path*",
-        destination: "http://127.0.0.1:5000/api/:path*",
+        destination: `${backendUrl}/api/:path*`,
       },
     ];
   },
